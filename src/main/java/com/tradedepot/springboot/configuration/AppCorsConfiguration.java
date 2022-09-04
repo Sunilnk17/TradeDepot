2b3086fe6a534770da2f96db265d32c5a4a378a8
sunilnk17 <sunilnkumaresan@gmail.com>
2022-09-04 13:40:57 +0200

    Conflict fixes

--- src/main/java/com/tradedepot/springboot/configuration/AppCorsConfiguration.java
+++ src/main/java/com/tradedepot/springboot/configuration/AppCorsConfiguration.java
@@ -1,10 +1,6 @@
 package com.tradedepot.springboot.configuration;
 
-
-<<<<<<< HEAD
-=======
 import org.springframework.beans.factory.annotation.Value;
->>>>>>> e4056e6 (Fixes to CORS endpoints)
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,23 +12,17 @@ import java.util.Collections;
 
 @Configuration
 public class AppCorsConfiguration {
-<<<<<<< HEAD
-=======
 
     @Value("${tdFrontEndUrl}")
     private String tdFrontEndUrl;
 
->>>>>>> e4056e6 (Fixes to CORS endpoints)
     @Bean
     public FilterRegistrationBean corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-<<<<<<< HEAD
         config.setAllowedOriginPatterns(Collections.singletonList("https://trade-depot-frontend-app.azurewebsites.net"));
-=======
         config.setAllowedOriginPatterns(Collections.singletonList(tdFrontEndUrl));
->>>>>>> e4056e6 (Fixes to CORS endpoints)
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
